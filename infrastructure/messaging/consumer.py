042df4b93e7fba3ce38d7118d9b620af3ffb6718
maksym.pestriakov <maksym.pestriakov@medtronic.com>
2026-02-13 17:55:03 +0200

    Fastapi initialisation

--- infrastructure/messaging/consumer.py
+++ infrastructure/messaging/consumer.py
@@ -3,40 +3,24 @@ import os
 
 import pika
 
-AMQP_URL = os.environ["AMQP_URL"]
+AMQP_URL = os.environ['AMQP_URL']
 
 params = pika.URLParameters(AMQP_URL)
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
-channel.exchange_declare(
-    exchange="dlx",
-    exchange_type="direct",
-    durable=True,
-)
-
-channel.queue_declare(
-    queue="task_queue.dlq",
-    durable=True,
-)
-
-channel.queue_bind(
-    exchange="dlx",
-    queue="task_queue.dlq",
-    routing_key="task_queue",
-)
-
+# Declare the general queue
 channel.exchange_declare(
     exchange='events',
     exchange_type='topic',
     durable=True)
 
 channel.queue_declare(
-    queue="task_queue",
+    queue='task_queue',
     durable=True,
     arguments={
-        "x-dead-letter-exchange": "dlx",
-        "x-dead-letter-routing-key": "task_queue",
+        'x-dead-letter-exchange': 'dlx',
+        'x-dead-letter-routing-key': 'task_queue',
     }
 )
 
@@ -46,10 +30,54 @@ channel.queue_bind(
     routing_key='task.*'
 )
 
+# Declare the DLX queue
+channel.exchange_declare(
+    exchange='dlx',
+    exchange_type='direct',
+    durable=True,
+)
+
+channel.queue_declare(
+    queue='task_queue.dlq',
+    durable=True,
+)
+
+channel.queue_bind(
+    exchange='dlx',
+    queue='task_queue.dlq',
+    routing_key='task_queue',
+)
+
+# Declare retry
+channel.exchange_declare(
+    exchange='retry',
+    exchange_type='direct',
+    durable=True,
+)
+
+channel.queue_declare(
+    queue='task_queue.retry',
+    durable=True,
+    arguments={
+        'x-message-ttl': 5000,  # 5 sec
+        'x-dead-letter-exchange': 'events',
+        'x-dead-letter-routing-key': 'task.created',
+    },
+)
+
+channel.queue_bind(
+    exchange='retry',
+    queue='task_queue.retry',
+    routing_key='task_queue',
+)
+
+
 def handle_message(ch, method, properties, body):
+    headers = properties.headers or {}
+    retries = headers.get('x-retry', 0)
     try:
         data = json.loads(body)
-        print(f'Processing: {data}')
+        print(f'Processing: {data}, attempts: {retries + 1}')
 
         if data['task_id'] == 1:
             raise RuntimeError('DB is down')
@@ -57,11 +85,28 @@ def handle_message(ch, method, properties, body):
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
     except Exception as e:
-        print(f'Error: {e}')
+        if retries < 2:
+            print('Retrying...')
+            
+            new_headers = headers | {'x-retry': retries + 1}
+            
+            ch.basic_publish(
+                exchange='retry',
+                routing_key='task_queue',
+                body=body,
+                properties=pika.BasicProperties(
+                    headers=new_headers,
+                    delivery_mode=2,
+                )
+            )
 
-        ch.basic_nack(delivery_tag=method.delivery_tag,
-                      requeue=False
-        )
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+        else:
+            print('Sending to DLQ')
+
+            ch.basic_nack(delivery_tag=method.delivery_tag,
+                          requeue=False
+            )
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='task_queue', on_message_callback=handle_message)
